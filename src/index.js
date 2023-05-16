85158d5fa66813c0930ad9fca805b3a9f43b0486
Sumanth <sumanth.koppula05@gmail.com>
2023-05-16 10:34:15 +0530

    Dispatch actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux'; 
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store',store);
+console.log('Before State', store.getState());
+
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name:'Superman'}]
+});
+
+console.log('After state',store.getState());
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
